12a30694e6d1dc13bddd27af9c172806c737df38
Alexa002 <aleksa.zlatkovic3i@gmail.com>
2023-04-11 18:39:41 +0200

    End of section 14

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using API.Entities;
 using Microsoft.EntityFrameworkCore;
 
-namespace API.Data 
+namespace API.Data
 {
     public class DataContext : DbContext
     {
@@ -13,5 +13,28 @@ namespace API.Data
         {
         }
         public DbSet<AppUser> Users { get; set; }
+        public DbSet<UserLike> Likes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<UserLike>()
+            .HasKey(k => new {
+                k.SourceUserId, k.TargetUserId} );
+
+            builder.Entity<UserLike>()
+            .HasOne(s=> s.SourceUser)
+            .WithMany(l=> l.LikedUsers)
+            .HasForeignKey(s=> s.SourceUserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+            
+            builder.Entity<UserLike>()
+            .HasOne(s=> s.TargetUser)
+            .WithMany(l=> l.LikedByUsers)
+            .HasForeignKey(s=> s.TargetUserId)
+            .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
